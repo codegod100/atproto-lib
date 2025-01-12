af3f269b6251cd3ffb690a4a31b611e595c0479f
nandi <78769380+codegod100@users.noreply.github.com>
2025-01-12 13:52:34 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# SvelteKit template with Tailwind and Typescript setup.
+You can use this template to instantly start working in a new codespace.
+
+## Commands used for setup
+
+```
+npm init vite ./
+```
+
+```
+npx svelte-add@latest tailwindcss
+```
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="flex w-full h-full items-center justify-center">
+    <h1 class="text-9xl">💩</h1>
+</div>
+
+<div class="flex absolute bottom-1 justify-center w-full">
+    <p class="text-sm text-gray-400 font-light tracking-wide">Jesper Dinger</p>
+</div>
